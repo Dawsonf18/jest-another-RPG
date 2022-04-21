a1f52108f85220a8b4cd5605127ca3dd840d09db
Dawson Filion <Dawsonfilion21@gmail.com>
2022-04-20 20:59:43 -0400

    update player js

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,3 +1,4 @@
+const { test } = require('picomatch');
 const Player = require('../lib/Player');
 
 const Potion = require('../lib/Potion');
@@ -17,4 +18,30 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test('gets players stats as an object', () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+});
+
+test('gets players health value', () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContainer(player.health.toString()));
+
 });
